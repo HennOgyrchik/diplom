459eb4fa4155910d41d578b14bd729084982ff32
Okiwa <okiwa@localhost.localdomain>
2023-12-07 15:41:46 +0300

    1

--- main/main.go
+++ main/main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"fmt"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"io/ioutil"
 	"log"
 	"os"
-
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"strconv"
 )
 
+var memory = map[int64]chan string{}
+
 func main() {
+
 	bot, err := tgbotapi.NewBotAPI(getToken())
 	if err != nil {
 		log.Panic(err)
@@ -25,13 +28,22 @@ func main() {
 	updates := bot.GetUpdatesChan(u)
 
 	for update := range updates {
-		if update.Message != nil { // If we got a message
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		if update.Message != nil {
+			if usrChan, ok := memory[update.Message.Chat.ID]; ok {
+				usrChan <- update.Message.Text
+			} else {
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+				go commandSwitcher(bot, &msg, update.Message.Command())
+			}
 
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-			msg.ReplyToMessageID = update.Message.MessageID
+		} else if update.CallbackQuery != nil {
+			if usrChan, ok := memory[update.CallbackQuery.Message.Chat.ID]; ok {
+				usrChan <- update.Message.Text
+			} else {
+				msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Message.Text)
 
-			bot.Send(msg)
+				go commandSwitcher(bot, &msg, update.CallbackQuery.Data)
+			}
 		}
 	}
 }
@@ -49,4 +61,89 @@ func getToken() string {
 		panic(err)
 	}
 	return string(token[:])
+}
+
+func commandSwitcher(bot *tgbotapi.BotAPI, msg *tgbotapi.MessageConfig, query string) {
+	var numericKeyboard = tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Создать", "create"),
+			tgbotapi.NewInlineKeyboardButtonData("Присоединиться", "join"),
+		),
+	)
+	switch query {
+
+	case "start":
+		msg.Text = "Приветствую! Выберите один из вариантов"
+		msg.ReplyMarkup = &numericKeyboard
+		if _, err := bot.Send(msg); err != nil {
+			panic(err)
+		}
+	case "create":
+		confirmationCreationNewFund(bot, msg.ChatID)
+	case "join":
+		msg.Text = "Присоединение"
+		if _, err := bot.Send(msg); err != nil {
+			panic(err)
+		}
+	case "Создать новый фонд":
+		creatingNewFund(bot, msg.ChatID)
+
+	case "Не создавать новый фонд":
+		msg.Text = "Не создаю"
+		if _, err := bot.Send(msg); err != nil {
+			panic(err)
+		}
+
+	default:
+		msg.Text = "Я не знаю такую команду"
+		if _, err := bot.Send(msg); err != nil {
+			panic(err)
+		}
+	}
+
+}
+
+func confirmationCreationNewFund(bot *tgbotapi.BotAPI, chatId int64) {
+	msg := tgbotapi.NewMessage(chatId, "Вы уверены, что хотите создать новый чат?")
+
+	var numericKeyboard = tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData("Да", "Создать новый фонд"),
+			tgbotapi.NewInlineKeyboardButtonData("Нет", "Не создавать новый фонд"),
+		),
+	)
+	msg.ReplyMarkup = numericKeyboard
+
+	if _, err := bot.Send(msg); err != nil {
+		panic(err)
+	}
+}
+
+func creatingNewFund(bot *tgbotapi.BotAPI, chatId int64) {
+	var err error
+	memory[chatId] = make(chan string)
+	fmt.Println(memory)
+	msg := tgbotapi.NewMessage(chatId, "Введите начальную сумму фонда без указания валюты. Например: 50.25")
+	if _, err = bot.Send(msg); err != nil {
+		panic(err)
+	}
+	var sum float64
+	for {
+		sum, err = strconv.ParseFloat(<-memory[chatId], 64)
+		if err != nil {
+			msg = tgbotapi.NewMessage(chatId, "Попробуйте еще раз")
+			if _, err = bot.Send(msg); err != nil {
+				panic(err)
+			}
+			continue
+		}
+		break
+	}
+	close(memory[chatId])
+	delete(memory, chatId)
+	msg = tgbotapi.NewMessage(chatId, strconv.FormatFloat(sum, 'f', -1, 64))
+	if _, err := bot.Send(msg); err != nil {
+		panic(err)
+	}
+	fmt.Println(memory)
 }
